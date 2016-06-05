b8744348aeaccc1f37ab0e0b8a40aeb3787413ba
hamaokah <strodr@gmail.com>
2016-06-05 18:14:21 +0900

    add request url func via func tapSearch

--- zipcode/ViewController.swift
+++ zipcode/ViewController.swift
@@ -15,6 +15,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var zipTextField: UITextField!
+
+    @IBAction func tapReturn() {
+    }
+
+    @IBAction func tapSearch() {
+        guard let ziptext = zipTextField.text else {
+            // End if value is nil
+            return
+        }
+        
+        // Create requested URL
+        let urlStr = "http://api.zipaddress.net/?zipcode=\(ziptext)"
+        print(urlStr)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
