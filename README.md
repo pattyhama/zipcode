d0f98e1901bbdd1d81cb6ee748776392bbb04af2
hamaokah <strodr@gmail.com>
2016-06-05 17:40:31 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Zip Code
+
+## Overview
+This is a Swift code for obtaining zip code.
+
+### System Requirements
+
+* OS X (10.11 / El Capitan) is the current supported operating systems
+* [Xcode (Version 7.0)](https://developer.apple.com/xcode/downloads/)
+
+* [Swift 2.0](http://www.apple.com/swift/)
---
+++ zipcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zipcode
+//
+//  Created by 浜岡春見 on 2016/06/05.
+//  Copyright © 2016年 pattyhama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
